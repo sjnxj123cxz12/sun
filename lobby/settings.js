8a5912b001a015a4aa2c04ea7b620be1e2a77d34
linhnhicute <99706641+linhnhicute@users.noreply.github.com>
2026-01-09 10:22:59 +0700

    1
    1

---
+++ lobby/settings.js
@@ -0,0 +1,1 @@
+{"platform":"ios","groupList":["default"],"collisionMatrix":[[true]],"hasResourcesBundle":false,"hasStartSceneBundle":false,"remoteBundles":false,"subpackages":[],"launchScene":"db://assets/MainScene.fire","orientation":"","server":"","jsList":[],"bundleVers":{"internal":"7b6e9","BanCa":"411d0","TaiXiu":"03438","lobby":"2aa3e","main":"b3713"}}
